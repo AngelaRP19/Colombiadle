1dadf2ee5f5f91ce1ccc2a0a3a662180886815af
Angela Peña <angela.pena05@uptc.edu.co>
2024-08-14 00:24:44 -0500

    Add function to delete and send box

--- js/main.js
+++ js/main.js
@@ -4,18 +4,45 @@ const keys = document.querySelectorAll('.letter');
 const table = document.querySelector('#table');
 const rows = table.querySelectorAll('.row');
 
+const send = document.querySelector("#write")
+const lastItem = document.querySelector("#delete")
+let word = "COLOMBIA"
+
 let initRowNum = 0;
 let initCellNum = 0;
 
+let actualRow 
 keys.forEach(key => {
     key.addEventListener('click', () => {
-        rows[initRowNum].querySelectorAll('div')[initCellNum].textContent =  key.textContent
-        i++
-        if(initCellNum === 8){
-            initCellNum = 0;
-            initRowNum++;
+        if(initCellNum !== 8){
+            actualRow = rows[initRowNum].querySelectorAll('div')
+            actualRow[initCellNum].textContent =  key.textContent
+            initCellNum++;
+            console.log(initCellNum)
         }
     });
 });
+
+lastItem.addEventListener('click', () => {
+    if(initCellNum > 0){
+        initCellNum--
+        actualRow[initCellNum].textContent = ""
+    }
+    })
+
+send.addEventListener('click', () => {
+    for (let i = 0; i < actualRow.length ; i++) {
+        if(actualRow[i].textContent === word[i]){
+            actualRow[i].classList.add('correct')
+        }else if(word.includes(actualRow[i].textContent)){
+            actualRow[i].classList.add('wrongPlace')
+        }else{
+            actualRow[i].classList.add('incorrect')
+        }
+        actualRow[i].classList.add('animation')
+      }
+    initCellNum = 0;
+    initRowNum++;
+    })
 
 
